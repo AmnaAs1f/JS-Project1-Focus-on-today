6a2fd565380df25eff65952c1b1eb1673b63410f
Amna Asif <asifamna920@gmail.com>
2024-05-19 00:10:13 +0500

    2ndcommit

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ const progressBar = document.querySelector('.progress-bar')
 const progressValue = document.querySelector('.progress-value')
 const progressLabel = document.querySelector('.progress-label')
 
-
+const allQuotes = [
+    'Raise the bar by completing your goals!',
+    'Well begun is half done!',
+    'Just a step away, keep going!',
+    'Whoa! You just completed all the goals, time for chill :D',
+  ]
 
 
 const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
 let completedGoalsCount =Object.values(allGoals).filter((goals) => goals.completed).length
 progressValue.style.width = `${completedGoalsCount/3 *100}`
 progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`
+
 
 checkBoxlist.forEach((checkbox) => {
 
@@ -28,7 +34,6 @@ checkBoxlist.forEach((checkbox) => {
             completedGoalsCount = Object.values(allGoals).filter((goals) => goals.completed).length
           progressValue.style.width = `${completedGoalsCount/3 *100}%`
           progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`
-
 
             localStorage.setItem('allGoals', JSON.stringify(allGoals))
         }
@@ -51,6 +56,12 @@ inputFields.forEach((input) => {
 
 
     input.addEventListener('input' , (e) => {
+        if(allGoals[input.id].completed){
+            input.value = allGoals[input.id].name
+            return
+        }
+
+
         allGoals[input.id]= {
             name: input.value,
             completed: false,
